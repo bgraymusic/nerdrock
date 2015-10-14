ef8e7d42ee1bfc0ddb85d09900ca7a2d146eb7ab
Brian Gray <brian_gray2@cable.comcast.com>
2015-10-13 20:26:40 -0400

    "social" rejiggering & start of badges code

--- js/bg.js
+++ js/bg.js
@@ -5,13 +5,59 @@ var discography;
 // If history.state exists, assume browser isn't going to fire initial popstate.
 var popped = ('state' in window.history && window.history.state !== null), initialURL = location.href;
 
+String.prototype.hashCode = function() {
+  var hash = 0, i, chr, len;
+  if (this.length == 0) return hash;
+  for (i = 0, len = this.length; i < len; i++) {
+    chr   = this.charCodeAt(i);
+    hash  = ((hash << 5) - hash) + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash;
+};
+
+var badgeSpec = {
+	// seamonkey
+	'1646334178': { id: 'jcc', img: 'jcc_boat.svg' },
+	// expecto
+	'-1309368522': { id: 'patreon', img: 'patreon_logo.png' },
+	// spintunes
+	'-2114035591': { id: 'spintunes', img: 'spintunes_starburst.gif' }
+};
+var badges = [];
+
+function assignBadges() {
+	// Fetch existing badges
+	if (typeof(Storage) !== 'undefined') {
+		if (!localStorage.getItem('badges')) localStorage.setItem('badges', JSON.stringify([]));
+		badges = JSON.parse(localStorage.getItem('badges'));
+	}
+
+	var newBadgesMaybe = $.url().param('badges') ? $.url().param('badges').split(',') : [];
+	var actualNewBadgeFound = false;
+	$.each(newBadgesMaybe, function() {
+		var hash = this.hashCode().toString();
+		if (badges.indexOf(hash) === -1 && badgeSpec[hash]) {
+			badges.push(hash);
+			alert(badgeSpec[hash].id + ' found');
+			actualNewBadgeFound = true;
+		}
+	});
+
+	if (typeof(Storage) !== 'undefined') {
+		localStorage.setItem('badges', JSON.stringify(badges));
+	} else if (actualNewBadgeFound) {
+		$('#bg-cannot-save-badges-alert').dialog('open');
+	}
+}
+
 function onDataComplete(bcData) {
 	new BG.Discography(bcData).buildDOM($('#'+BG.Discography.css.cont));
 	registerGlobalJQueryUI();
 	BG.Discography.registerJQueryUI();
 
-// 	var startBlog = $.url().param('blog') ? $.url().param('blog') : ' ';
-// 	navigate({ toptab: $.url().param('toptab'), song: $.url().param('song'), songtab: $.url().param('songtab'), blog: startBlog });
+	assignBadges();
+
 	navigate({
 		toptab: $.url().param('toptab'), blog: $.url().param('blog'),
 		song: $.url().param('song'), songtab: $.url().param('songtab')
@@ -25,15 +71,19 @@ function registerGlobalJQueryUI() {
 	$('#bg-github').repo({ user: 'bgraymusic', name: 'nerdrock' });
 	$(window).bind('popstate', function(event) {
 		// Ignore inital popstate that some browsers fire on page load
-		var initialPop = !popped && location.href == initialURL
-		popped = true
-		if (initialPop) return;
-
-		navigate({
-			toptab: $.url().param('toptab'), blog: $.url().param('blog'),
-			song: $.url().param('song'), songtab: $.url().param('songtab')
-		});
+		var initialPop = !popped && location.href == initialURL;
+		popped = true;
+		if (!initialPop) {
+			navigate({
+				toptab: $.url().param('toptab'), blog: $.url().param('blog'),
+				song: $.url().param('song'), songtab: $.url().param('songtab')
+			});
+		}
 	});
+	$('#bg-cannot-save-badges-alert').dialog({ autoOpen: false, resizable: false, modal: true, buttons: {
+		'Don\'t tell me what to do': function() { $(this).dialog('close'); },
+		'Ok': function() { $(this).dialog('close'); }
+	}});
 }
 
 function saveState() {
